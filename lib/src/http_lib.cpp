b28f43b90c6eea6db915230aac1129ad46c28f75
GameDev4Fun <valere@gamedev4fun.io>
2023-03-31 14:47:22 -0400

    Add FMT lib

--- lib/src/http_lib.cpp
+++ lib/src/http_lib.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+#include <fmt/format.h>
+
 #include "my_http_lib/http_lib.h"
 
 namespace my_http_lib
 {
     void test()
     {
-        std::cout << "Hello world!!!" << std::endl;
+        std::cout << fmt::format("Hello World from {}", "toto") << std::endl;
     }
 }
